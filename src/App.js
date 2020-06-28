22d6b0afda666e50676b354b87cc240c746a7120
SauravKanchan <sauravnk30@gmail.com>
2020-06-28 20:14:37 +0530

    Fix some logo

--- src/App.js
+++ src/App.js
@@ -10,33 +10,42 @@ function App() {
     const fetchChannels = async () => {
       let res = await axios.get('https://raw.githubusercontent.com/billacablewala/m3u8/master/README.md')
       let reader = new m3u8Parser()
-      let urls = res.data.split('\n')
-      urls.forEach((url) => {
+      let urls = res.data.
+      replace(/= "/g, '="').
+      replace(/EXTINF:0,/g, 'EXTINF:0 ').
+      replace(/EXTINF:-1,/g, 'EXTINF:-1 ').
+      replace(/ttvg-logo/g, 'tvg-logo').
+      split("#EXTINF:");
+
+      urls.forEach((url, i) => {
         try {
-          reader.read(url)
+          if(i===0){
+            reader.read(url)
+          }else {
+            reader.read("#EXTINF:"+url)
+          }
         } catch (error) {
           console.log('Error in reading url')
         }
-      })
+      });
       setChannels(reader.getResult().segments)
       console.log(reader.getResult().segments)
-    }
+    };
     try {
       fetchChannels()
     } catch (error) {
       console.log('Error in fetching channels')
     }
-  }, [])
+  }, []);
   return (
     <div className="app">
       {channels &&
         channels.map((channel) => {
-          console.log(channel)
           return (
             <img
               style={{ height: '50px', width: '50px' }}
-              src={channel.inf && channel.inf.tvgLogo}
-              alt="channel-logo"
+              src={channel.inf && (channel.inf.tvgLogo || channel.inf.logo)}
+              alt={channel.inf && channel.inf.title}
             />
           )
         })}
