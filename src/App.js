0514b8f28fbebcdfb0ab1b8423f5dd7ecbfd15a7
Faraz Khan <farazrk001@gmail.com>
2020-06-28 20:08:27 +0530

    Add channel cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import logo from './logo.svg'
 import axios from 'axios'
 import m3u8Parser from 'm3u8-file-parser'
 import './App.css'
@@ -28,18 +27,24 @@ function App() {
     }
   }, [])
   return (
-    <div className="app">
-      {channels &&
-        channels.map((channel) => {
-          console.log(channel)
-          return (
-            <img
-              style={{ height: '50px', width: '50px' }}
-              src={channel.inf && channel.inf.tvgLogo}
-              alt="channel-logo"
-            />
-          )
-        })}
+    <div className="max-w-6xl mx-auto ">
+      <header className="px-4 text-2xl font-bold">IPTV</header>
+      <div className="flex flex-wrap justify-between">
+        {channels &&
+          channels.map((channel) => {
+            console.log(channel)
+            return (
+              <div>
+                <img
+                  className="h-32 p-4 m-2 rounded shadow"
+                  src={channel.inf && channel.inf.tvgLogo}
+                  alt="channel-logo"
+                />
+                <p className="px-4">{channel.inf && channel.inf.title}</p>
+              </div>
+            )
+          })}
+      </div>
     </div>
   )
 }
