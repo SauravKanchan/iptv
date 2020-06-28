dea3ea4b3f1527222a30eb9c16fd9ef40d954ca5
Faraz Khan <farazrk001@gmail.com>
2020-06-28 20:34:07 +0530

    Merge changes and enforce eslint

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css'
 import brokenImg from './img/brokenimage.png'
 
 const ChannelImage = ({ channel }) => {
-  const [source, setSource] = useState(channel.inf && (channel.inf.tvgLogo || channel.inf.logo));
+  const [source, setSource] = useState(channel.inf && (channel.inf.tvgLogo || channel.inf.logo))
   return (
     <img
       className="h-32 m-2 rounded shadow max-w-32"
@@ -14,41 +14,47 @@ const ChannelImage = ({ channel }) => {
       onError={() => setSource(brokenImg)}
     />
   )
-};
+}
 
 function App() {
   const [channels, setChannels] = useState([])
   useEffect(() => {
     const fetchChannels = async () => {
-      let res = await axios.get('https://raw.githubusercontent.com/billacablewala/m3u8/master/README.md');
+      let res = await axios.get('https://raw.githubusercontent.com/billacablewala/m3u8/master/README.md')
       let reader = new m3u8Parser()
-      let urls = res.data.replace(/= "/g, '="').replace(/EXTINF:0,/g, 'EXTINF:0 ').replace(/EXTINF:-1,/g, 'EXTINF:-1 ').replace(/ttvg-logo/g, 'tvg-logo').replace(/tvg-logo"" ,/g, 'tvg-logo"" ').split("#EXTINF:");
+      let urls = res.data
+        .replace(/= "/g, '="')
+        .replace(/EXTINF:0,/g, 'EXTINF:0 ')
+        .replace(/EXTINF:-1,/g, 'EXTINF:-1 ')
+        .replace(/ttvg-logo/g, 'tvg-logo')
+        .replace(/tvg-logo"" ,/g, 'tvg-logo"" ')
+        .split('#EXTINF:')
 
       urls.forEach((url, i) => {
         try {
           if (i === 0) {
             reader.read(url)
           } else {
-            reader.read("#EXTINF:" + url)
+            reader.read('#EXTINF:' + url)
           }
         } catch (error) {
           console.log('Error in reading url')
         }
-      });
-      setChannels(reader.getResult().segments);
-      let parsed = reader.getResult().segments;
+      })
+      setChannels(reader.getResult().segments)
+      let parsed = reader.getResult().segments
       parsed.forEach((d) => {
         if (!(d.inf && (d.inf.tvgLogo || d.inf.logo))) {
           console.log(d)
         }
       })
-    };
+    }
     try {
       fetchChannels()
     } catch (error) {
       console.log('Error in fetching channels')
     }
-  }, []);
+  }, [])
   return (
     <div className="max-w-6xl mx-auto ">
       <div className="flex justify-between py-8">
