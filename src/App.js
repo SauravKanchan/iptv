a2968e1ec39b841f7f2da0504cec945e722dc48c
Faraz Khan <farazrk001@gmail.com>
2020-06-30 19:12:15 +0530

    Layout improvements

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import urls from './m3u8.json'
 
 function App() {
   const [channels, setChannels] = useState([])
-  const [url, setUrl] = useState()
+  const [url, setUrl] = useState(null)
+  const [searchText, setSearchText] = useState('')
 
   const ChannelImage = ({ channel }) => {
     const [source, setSource] = useState(channel.tvgLogo)
@@ -40,10 +41,10 @@ function App() {
     }
   }
 
-  async function search(event) {
+  async function search() {
     let filtered_channels = []
-    if (event.target.value) {
-      let result = await fuzzysort.goAsync(event.target.value, urls, {
+    if (searchText) {
+      let result = await fuzzysort.goAsync(searchText, urls, {
         limit: 15,
         key: 'title',
         allowTypo: false,
@@ -67,20 +68,32 @@ function App() {
       console.log('Error in fetching channels')
     }
   }, [])
+
+  useEffect(() => search(), [searchText])
   return (
     <div className="max-w-6xl mx-auto ">
       <div className="flex justify-between py-8">
-        <header className="px-4 text-2xl font-bold">IPTV</header>
+        <header className="sticky top-0 px-4 text-2xl font-bold">IPTV</header>
         <input
           type="text"
           alt="search"
           className="px-4 bg-gray-100 border rounded-full"
           placeholder="Search for a channel"
-          onChange={search}
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
         />
       </div>
-      {url && <ReactPlayer playing controls url={url} />}
-      <div className="grid grid-cols-8">
+      {url && (
+        <div className="flex justify-between mx-2 my-8 bg-gray-100">
+          <div className="flex flex-col justify-center">
+            <h1 className="hidden px-4 py-8 text-4xl font-bold md:block">Channel Name</h1>
+            <img src="#" alt="" className="block w-20 h-20 px-4 bg-gray-800 rounded" />
+          </div>
+          <ReactPlayer playing controls url={url} />{' '}
+        </div>
+      )}
+      <h1 className="px-2 text-2xl">{searchText ? 'All Channels' : `Showing Results for ${searchText}`}</h1>
+      <div className="grid grid-cols-4 md:grid-cols-8">
         {channels &&
           channels.map((channel) => {
             return (
